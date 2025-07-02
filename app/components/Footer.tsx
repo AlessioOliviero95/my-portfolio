10dbc14f52b41e7471ce533952d80f4b92abd5da
AlessioOliviero <alessiooliviero95@gmail.com>
2025-07-02 12:56:32 +0200

    hamburgher menu

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
       <footer className="border-t py-4 mt-10 text-center text-sm text-muted-foreground">
-        &copy; {new Date().getFullYear()} Luca. All rights reserved.
+        &copy; {new Date().getFullYear()} Alessio Oliviero. All rights reserved.
       </footer>
     );
   }
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 export default function HomePage() {
   return (
-    <section className="space-y-6">
-      <h1 className="text-4xl font-bold">Hi, I'm Luca 👋</h1>
-      <p className="text-lg text-muted-foreground">
+    <section className="flex flex-col items-center justify-center space-y-2 sm:space-y-4 md:space-y-6">
+      <h1 className="text-xl sm:text-2xl md:text-4xl font-bold">Hi, I'm Alessio Oliviero 👋</h1>
+      <p className="text-sm sm:text-lg text-center  text-muted-foreground">
         Front-End Developer with 5+ years of experience building beautiful, performant web apps.
       </p>
     </section>
